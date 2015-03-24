24d9f934937a201d21b8ebda65c94573e33ab80f
Takemi Watanuki <watakemi725@Takemi-no-MacBook-Pro.local>
2015-03-24 09:53:30 +0900

    hello

---
+++ GachaTech/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  GachaTech
+//
+//  Created by Takemi Watanuki on 2015/03/21.
+//  Copyright (c) 2015年 your name. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func gacha() {
+        
+        self.performSegueWithIdentifier("result", sender: nil)
+        
+    }
+
+}
+
